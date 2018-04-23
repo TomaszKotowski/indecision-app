63da260b26b644ddb7b35cc01ab9b7b530ba0c29
TomaszKotowski <tomaszk72@gmail.com>
2018-04-23 21:55:52 +0200

    Almost all styling done, only the modal is left

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -65,11 +65,13 @@ export default class IndesicionApp extends React.Component {
                     <Action
                         hasOptions={this.state.options.length > 0}
                         handlePick={this.handlePick} />
-                    <Options
-                        options={this.state.options}
-                        handleDeleteOptions={this.handleDeleteOptions}
-                        handleDeleteOption={this.handleDeleteOption} />
-                    <AddOption handleAddOption={this.handleAddOption} />
+                    <div className="widget" >
+                        <Options
+                            options={this.state.options}
+                            handleDeleteOptions={this.handleDeleteOptions}
+                            handleDeleteOption={this.handleDeleteOption} />
+                        <AddOption handleAddOption={this.handleAddOption} />
+                    </div>
                     <OptionModal
                         selectedOption={this.state.selectedOption}
                         handleClearSelectedOption={this.handleClearSelectedOption} />
--- src/components/Options.js
+++ src/components/Options.js
@@ -3,17 +3,21 @@ import Option from './Option.js'
 
 const Options = (props) => (
     <div>
-        <button
-            className="button button--link"
-            onClick={props.handleDeleteOptions}
-        >Remove All
+        <div className="widget-header">
+            <h3 className="widget-header__title" >Your options</h3>
+            <button
+                className="button button--link"
+                onClick={props.handleDeleteOptions}
+            >Remove All
         </button>
-        {props.options.length === 0 && <p>Please add an option to get stated</p>}
+        </div>
+        {props.options.length === 0 && <p className="widget-messege">Please add an option to get stated</p>}
         {
-            props.options.map((option) => (
+            props.options.map((option, index) => (
                 <Option
                     key={option}
                     optionText={option}
+                    count={index + 1}
                     handleDeleteOption={props.handleDeleteOption}
                 />)
             )
