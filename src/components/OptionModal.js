fc87ae015248498d28468b79d961c12b2d52d9aa
TomaszKotowski <tomaszk72@gmail.com>
2018-04-25 23:13:23 +0200

    End of section 8

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,9 +6,11 @@ const OptionModal = (props)=>(
         isOpen={!!props.selectedOption}
         onRequestClose={props.handleClearSelectedOption}
         contentLabel = "Selected option"
+        closeTimeoutMS = {200}
+        className="modal"
     > 
-        <h3>Selected option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
+        <h3 className="modal__title" >Selected option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
         <button onClick ={props.handleClearSelectedOption}>Okay</button>
     </Modal>
 );
